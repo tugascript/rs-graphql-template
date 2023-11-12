490d206c7b21b96e7a620e3ac609397502694e75
Afonso Barracha <barracha.afonso@gmail.com>
2023-11-12 21:25:44 +1300

    fix: fix auth guard

--- src/guards/auth_guard.rs
+++ src/guards/auth_guard.rs
@@ -4,14 +4,15 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use async_graphql::{Error, Guard, Result};
+use async_graphql::{async_trait, Context, Error, Guard, Result};
 
 use crate::common::AuthTokens;
 
 pub struct AuthGuard;
 
+#[async_trait::async_trait]
 impl Guard for AuthGuard {
-    fn check(&self, ctx: &async_graphql::Context<'_>) -> Result<()> {
+    async fn check(&self, ctx: &Context<'_>) -> Result<()> {
         let tokens = ctx.data::<AuthTokens>()?;
 
         if tokens.access_token.is_none() {
